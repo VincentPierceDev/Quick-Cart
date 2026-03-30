90466372fcbd0428abf888d7fad056e2562ff4b2
Vincent Pierce <VincentPierce.dev@gmail.com>
2026-03-30 13:06:53 -0500

    created the shop component to hydrate data properly

--- quick-cart/app/page.tsx
+++ quick-cart/app/page.tsx
@@ -1,9 +1,6 @@
 import { ItemData } from "@/global-types/shop/shop-types";
 import { RetrieveAllShopItems } from "@/lib/shop/shop-services";
-import { ShopDataProvider } from "@/contexts/shop/shop-provider";
-import ShoppingGrid from "@/components/shopping/shopping-grid";
-import ShoppingCart from "@/components/shopping/cart/shopping-cart";
-import { CartDataProvider } from "@/contexts/shop/cart-provider";
+import Shop from "@/components/shopping/Shop";
 
 //NOTE: This would typically by at like a specific route
 //however, I am just adding it as Next's default home page for simplicity of this project
@@ -12,13 +9,6 @@ export default async function Home() {
   const storeItems: ItemData[] = await RetrieveAllShopItems();
 
   return (
-    <>
-      <ShopDataProvider initialProducts={storeItems}>
-      <CartDataProvider>
-          <ShoppingGrid/>
-          <ShoppingCart/> 
-      </CartDataProvider>
-      </ShopDataProvider>
-    </>
+    <Shop data={storeItems}/>
   );
 }
---
+++ quick-cart/components/shopping/Shop.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import ShoppingCart from "./cart/shopping-cart"
+import ShoppingGrid from "./shopping-grid"
+import { ItemData } from "@/global-types/shop/shop-types"
+import { ShopDataProvider } from "@/contexts/shop/shop-provider"
+import { CartDataProvider } from "@/contexts/shop/cart-provider"
+
+interface Props {
+    data: ItemData[]
+}
+
+export default function Shop(props: Props) {
+    return(
+        <>
+      <ShopDataProvider initialProducts={props.data}>
+      <CartDataProvider>
+          <ShoppingGrid/>
+          <ShoppingCart/> 
+      </CartDataProvider>
+      </ShopDataProvider>
+    </>
+    )
+}
