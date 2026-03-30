2d906a0894fc4ede2caaf58c2c6a36ce0bda7963
Vincent Pierce <VincentPierce.dev@gmail.com>
2026-03-30 12:19:23 -0500

    updated next config

--- quick-cart/next.config.ts
+++ quick-cart/next.config.ts
@@ -12,7 +12,9 @@ const nextConfig: NextConfig = {
     ]
   },
 
-  allowedDevOrigins: ["192.168.10.2"],
+  ...(process.env.NODE_ENV === 'development' && {
+    allowedDevOrigins: ["192.168.10.2"],
+  })
 };
 
 export default nextConfig;
