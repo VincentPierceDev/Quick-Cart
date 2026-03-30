74eda2a9575eb772ad80d44b51a9c498e1fb02b3
Vincent Pierce <VincentPierce.dev@gmail.com>
2026-03-30 13:37:24 -0500

    more debugging added, potential fix with dynamic

--- quick-cart/app/page.tsx
+++ quick-cart/app/page.tsx
@@ -5,8 +5,11 @@ import Shop from "@/components/shopping/Shop";
 //NOTE: This would typically by at like a specific route
 //however, I am just adding it as Next's default home page for simplicity of this project
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const storeItems: ItemData[] = await RetrieveAllShopItems();
+  console.log(storeItems.length);
 
   return (
     <Shop data={storeItems}/>
