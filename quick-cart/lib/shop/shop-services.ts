ffc6d5677c3e00480a953e0a6f40bd7c86918a90
Vincent Pierce <VincentPierce.dev@gmail.com>
2026-03-30 13:47:54 -0500

    added more to the user agent

--- quick-cart/lib/shop/shop-services.ts
+++ quick-cart/lib/shop/shop-services.ts
@@ -10,7 +10,7 @@ export async function RetrieveAllShopItems() {
         const response = await fetch(endpoint, {
             cache: 'default',
             headers: {
-                'User-Agent': 'Mozilla/5.0'
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
             }
         });
 
